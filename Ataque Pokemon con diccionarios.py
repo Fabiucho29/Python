e1a9d106f582a0c4e1bf92ff189cd0baad8b9f8e
Fabiucho29 <152040508+Fabiucho29@users.noreply.github.com>
2024-01-25 17:43:50 -0400

    Add files via upload

---
+++ Ataque Pokemon con diccionarios.py
@@ -0,0 +1,77 @@
+import random
+
+Ps_jugador = 100
+Ps_oponente = 100
+defensa_oponente = 100
+defensa_jugador = 100
+
+historial_ataques = []
+
+while Ps_jugador > 0 and Ps_oponente > 0: 
+
+    print("La vida del jugador es", Ps_jugador)
+
+    print("La defensa del jugador es", defensa_jugador)
+
+    print("La vida del oponente es", Ps_oponente)
+
+    print ("La defensa del oponente es", defensa_oponente)
+
+    ataque_jugador = input("Ataque: ")
+    ataque_jugador = ataque_jugador.lower()
+
+    if ataque_jugador == "malicioso":
+        defensa_oponente -= 10
+        if defensa_oponente <= 0:
+            defensa_oponente = 1
+    
+    elif ataque_jugador == "placaje":
+        Ps_oponente -= (35 / (defensa_oponente/100))
+
+    elif ataque_jugador == "ascuas":
+        Ps_oponente -= 20
+    
+    else:
+        print ("Qué estas haciendo? Tus ataques son malicioso, placaje y ascuas")
+        continue
+
+
+    historial_ataques.append(ataque_jugador)
+
+    # Turno del Oponente
+    # El turno del oponente se define como un Random
+
+    ataque_oponente = random.randrange (1, 3+1)
+
+    if ataque_oponente == 1: #latigo
+        print ("Te ataco con látigo")
+        defensa_jugador -= 10
+        if defensa_jugador <= 0:
+            defensa_jugador = 1
+
+    elif ataque_oponente == 2: #placaje
+        print("Te ataco con placaje")
+        Ps_jugador -= (35 / (defensa_jugador/100))
+
+    else: #pistola de agua
+        print("Te ataco con pistola de agua")
+        Ps_jugador -= 40
+
+if Ps_jugador <= 0:
+    if Ps_oponente <= 0:
+        print ("Hubo un empate. Se decidirá en la revancha")
+
+    else:
+        print ("Has perdido, suerte la próxima")
+
+else: 
+    print ("Felicidades, eres un gran maestro Pokemon")
+
+print("Por último, este fue tu historial de ataques durante la partida:")
+
+diccionario_ataques = {}
+
+for i in range (len(historial_ataques)):
+    diccionario_ataques[f"Ataque {i + 1}"] = historial_ataques[i]
+
+print(diccionario_ataques)
